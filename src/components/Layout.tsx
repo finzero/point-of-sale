8671ef427dcd6a2a584f29be2c25ad3fb8ecc8c3
Fazrin <fazrin.mutaqin@wgs-id.com>
2023-03-28 16:50:27 +0700

    fix: Layout responsive mobile

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,11 +2,11 @@ import { Outlet } from 'react-router-dom';
 import { Header } from './Header';
 
 export const Layout = () => (
-  <div className="relative flex flex-row min-h-screen">
+  <div className="relative w-full flex flex-row min-h-screen">
     <nav className="w-full fixed top-0 left-0 h-10 bg-orange-500 drop-shadow-lg py-2 z-10 px-5">
       <Header />
     </nav>
-    <div className="mt-10">
+    <div className="mt-10 w-full">
       <Outlet />
     </div>
   </div>
