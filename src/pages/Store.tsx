1b10b34baf0e16e065d1f027612f32259edb7bad
Fazrin <fazrin.mutaqin@wgs-id.com>
2023-03-28 14:56:43 +0700

    chore: Move product data into json

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,21 +1,6 @@
 import { Cart } from '../components/Cart';
 import { ProductList } from '../components/ProductList';
-import { IProduct } from '../models/application.model';
-
-const products: IProduct[] = [
-  { id: 1, name: 'Sushi sapi', price: 10000 },
-  { id: 2, name: 'Sushi ayam', price: 10000 },
-  { id: 3, name: 'Sushi bebek', price: 90000 },
-  { id: 4, name: 'Sushi Kambing', price: 15000 },
-  { id: 5, name: 'Sushi susanti', price: 365000 },
-  { id: 6, name: 'Sushi domba', price: 80000 },
-  { id: 7, name: 'Sushi kerbau', price: 80000 },
-  { id: 8, name: 'Sushi buaya', price: 80000 },
-  { id: 9, name: 'Sushi cacing', price: 80000 },
-  { id: 10, name: 'Sushi cicak', price: 80000 },
-  { id: 11, name: 'Sushi ular', price: 80000 },
-  { id: 12, name: 'Sushi tikus', price: 80000 },
-];
+import products from '../assets/json/products.json';
 
 export const Store = () => {
   return (
@@ -24,7 +9,7 @@ export const Store = () => {
         role="main"
         className="md:w-[calc(100vw-300px)] w-full p-4 bg-yellow-50 min-h-full"
       >
-        <ProductList products={products} />
+        <ProductList products={products.data} />
       </main>
       <aside className="fixed top-8 right-0 w-[300px] bg-orange-100 hidden md:block min-h-full overflow-auto">
         <Cart />
