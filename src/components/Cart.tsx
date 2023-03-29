cab64cf321071f2dbdabd01976b10fcdd9cf60c7
Fazrin <fazrin.mutaqin@wgs-id.com>
2023-03-29 17:09:45 +0700

    feat: Cart - Implement Popup cancel confirmation

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -4,11 +4,22 @@ import { thousandSeparator } from '../utilities/formatter';
 import CartItem from './CartItem';
 import { useCartAction, useCartItems } from '../store/cartStore';
 import Button from './Button';
+import { PopupConfirm, PopupConfirmProps } from './PopupConfirm';
+
+const initialConfirmConfig = {
+  show: false,
+  title: '',
+  description: '',
+  confirm: () => {},
+  cancel: () => {},
+};
 
 export const Cart = () => {
   const items = useCartItems();
   const { clearCart } = useCartAction();
-  const [subtotal, setSubtotal] = useState(0);
+  const [subtotal, setSubtotal] = useState<number>(0);
+  const [confirmConfig, setConfirmConfig] =
+    useState<PopupConfirmProps>(initialConfirmConfig);
 
   useEffect(() => {
     const total = items.reduce((p, n) => {
@@ -17,21 +28,31 @@ export const Cart = () => {
     setSubtotal(total);
   }, [items]);
 
-  function bayar() {
-    alert(`Bayar`);
+  function handlePay() {}
+
+  function cancelConfirmation() {
+    setConfirmConfig({
+      show: true,
+      title: 'Konfirmasi Pembatalan',
+      description: 'Anda ingin membatalkan Pesanan',
+      confirm: handleClearCart,
+      cancel: closeModal,
+    });
+  }
+
+  function handleClearCart() {
+    clearCart();
+    closeModal();
+  }
+
+  function closeModal() {
+    setConfirmConfig({ ...initialConfirmConfig, show: false });
   }
 
   return (
     <div className="p-4 w-full max-h-screen overflow-auto">
       <div className="flex justify-between p-2 h-10 w-full mb-2">
         <div className="text-black font-bold">Total Barang: {items.length}</div>
-        <button
-          type="button"
-          onClick={clearCart}
-          className="text-white drop-shadow-lg bg-orange-700 font-bold px-2 py-1 rounded-md w-8 h-8 pt-2"
-        >
-          <i className="fi fi-ss-trash"></i>
-        </button>
       </div>
       <ul className="flex flex-col gap-2">
         {items.map((item: ICartItem) => (
@@ -42,7 +63,14 @@ export const Cart = () => {
         <span>Subtotal:</span>
         <span>{thousandSeparator(subtotal)}</span>
       </div>
-      <Button label="Bayar" click={bayar} disabled={items.length === 0} />
+      <Button label="Bayar" click={handlePay} disabled={!items.length} />
+      <Button
+        disabled={!items.length}
+        label="Batal"
+        click={cancelConfirmation}
+        style={{ bg: 'bg-red-500', color: 'text-white' }}
+      />
+      <PopupConfirm {...confirmConfig} />
     </div>
   );
 };
