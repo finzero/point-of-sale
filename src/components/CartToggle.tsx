440411519ccdb264132a83ef299fef15e4bfe435
Fazrin <fazrin.mutaqin@wgs-id.com>
2023-04-11 17:07:29 +0700

    chore: add toggle slide animation

--- src/components/CartToggle.tsx
+++ src/components/CartToggle.tsx
@@ -1,13 +1,20 @@
+import { useCartItems } from '../store/cartStore';
+
 interface CartToggleProps {
   click: () => void;
 }
 const CartToggle = ({ click }: CartToggleProps) => {
+  const items = useCartItems();
+
   return (
     <div
-      className="sm:block md:hidden absolute top-6 -left-9 h-10 w-10 rounded-l-xl bg-orange-300 drop-shadow-lg flex justify-center items-center"
+      className="md:hidden cursor-pointer absolute top-6 -left-9 h-10 w-10 rounded-l-xl bg-orange-300 drop-shadow-lg flex justify-center items-center"
       onClick={click}
     >
       <i className="text-white fi fi-ss-shopping-cart text-lg"></i>
+      <span className="bg-red-500 text-white font-bold absolute rounded-full -top-2 -left-2 w-5 h-5 text-center text-xs">
+        {items.length}
+      </span>
     </div>
   );
 };
