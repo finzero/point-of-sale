d48017e4d610d66771475bfb644c42686c3b63f6
Fazrin <fazrin.mutaqin@wgs-id.com>
2023-04-11 17:07:47 +0700

    Products - lift some functions

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -2,25 +2,18 @@ import { useEffect, useState } from 'react';
 import { ICartItem } from '../models/application.model';
 import { thousandSeparator } from '../utilities/formatter';
 import CartItem from './CartItem';
-import { useCartAction, useCartItems } from '../store/cartStore';
+import { useCartItems } from '../store/cartStore';
 import Button from './Button';
-import { PopupConfirm, PopupConfirmProps } from './PopupConfirm';
 import CartToggle from './CartToggle';
 
-const initialConfirmConfig = {
-  show: false,
-  title: '',
-  description: '',
-  confirm: () => {},
-  cancel: () => {},
-};
+interface CartProps {
+  handlePay: () => void;
+  handleCancel: () => void;
+}
 
-export const Cart = () => {
+export const Cart = ({ handlePay, handleCancel }: CartProps) => {
   const items = useCartItems();
-  const { clearCart } = useCartAction();
   const [subtotal, setSubtotal] = useState<number>(0);
-  const [confirmConfig, setConfirmConfig] =
-    useState<PopupConfirmProps>(initialConfirmConfig);
 
   useEffect(() => {
     const total = items.reduce((p, n) => {
@@ -28,35 +21,6 @@ export const Cart = () => {
     }, 0);
     setSubtotal(total);
   }, [items]);
-
-  function handlePay() {
-    setConfirmConfig({
-      show: true,
-      title: 'Informasi',
-      description: 'Maaf Blom Bisa Bayar, blom ready fitur-nya',
-      confirm: closeModal,
-      cancel: closeModal,
-    });
-  }
-
-  function cancelConfirmation() {
-    setConfirmConfig({
-      show: true,
-      title: 'Konfirmasi Pembatalan',
-      description: 'Anda ingin membatalkan Pesanan',
-      confirm: handleClearCart,
-      cancel: closeModal,
-    });
-  }
-
-  function handleClearCart() {
-    clearCart();
-    closeModal();
-  }
-
-  function closeModal() {
-    setConfirmConfig({ ...initialConfirmConfig, show: false });
-  }
 
   const isSmallDevice = window.innerWidth <= 450;
   const [showCart, setShowCart] = useState(!isSmallDevice);
@@ -66,31 +30,36 @@ export const Cart = () => {
   }
 
   return (
-    <div className="max-h-[calc(100vh-40px)] overflow-auto">
-      <CartToggle click={toggleCart} />
-      <div className={`${showCart ? 'w-[300px] p-4' : 'w-0'}`}>
-        <div className="flex justify-between p-2 h-10 w-full mb-2">
-          <div className="text-black font-bold">
-            Total Barang: {items.length}
+    <div
+      className={`fixed top-8 -right-[300px] duration-500 bg-orange-100 md:block min-h-full ${
+        showCart ? '-translate-x-[300px]' : ''
+      }`}
+    >
+      <div className="max-h-[calc(100vh-40px)] overflow-auto">
+        <CartToggle click={toggleCart} />
+        <div className="w-[300px] p-4">
+          <div className="flex justify-between p-2 h-10 w-full mb-2">
+            <div className="text-black font-bold">
+              Total Barang: {items.length}
+            </div>
           </div>
+          <ul className="flex flex-col gap-2">
+            {items.map((item: ICartItem) => (
+              <CartItem item={item} key={item.id} />
+            ))}
+          </ul>
+          <div className="rounded-md bg-white p-4 mt-2 text-orange-400 font-bold flex justify-between">
+            <span>Subtotal:</span>
+            <span>{thousandSeparator(subtotal)}</span>
+          </div>
+          <Button label="Bayar" click={handlePay} disabled={!items.length} />
+          <Button
+            disabled={!items.length}
+            label="Batal"
+            click={handleCancel}
+            style={{ bg: 'bg-red-500', color: 'text-white' }}
+          />
         </div>
-        <ul className="flex flex-col gap-2">
-          {items.map((item: ICartItem) => (
-            <CartItem item={item} key={item.id} />
-          ))}
-        </ul>
-        <div className="rounded-md bg-white p-4 mt-2 text-orange-400 font-bold flex justify-between">
-          <span>Subtotal:</span>
-          <span>{thousandSeparator(subtotal)}</span>
-        </div>
-        <Button label="Bayar" click={handlePay} disabled={!items.length} />
-        <Button
-          disabled={!items.length}
-          label="Batal"
-          click={cancelConfirmation}
-          style={{ bg: 'bg-red-500', color: 'text-white' }}
-        />
-        <PopupConfirm {...confirmConfig} />
       </div>
     </div>
   );
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,53 @@
 import { Cart } from '../components/Cart';
 import { ProductList } from '../components/ProductList';
 import products from '../assets/json/products.json';
+import { useState } from 'react';
+import { PopupConfirm, PopupConfirmProps } from '../components/PopupConfirm';
+import { useCartAction } from '../store/cartStore';
+
+const initialConfirmConfig = {
+  show: false,
+  title: '',
+  description: '',
+  confirm: () => {},
+  cancel: () => {},
+};
 
 export const Store = () => {
+  const [confirmConfig, setConfirmConfig] =
+    useState<PopupConfirmProps>(initialConfirmConfig);
+
+  function handlePay() {
+    setConfirmConfig({
+      show: true,
+      title: 'Informasi',
+      description: 'Payment feature is on progress',
+      confirm: closeModal,
+      cancel: closeModal,
+    });
+  }
+
+  function handleCancel() {
+    setConfirmConfig({
+      show: true,
+      title: 'Konfirmasi Pembatalan',
+      description: 'Anda ingin membatalkan Pesanan',
+      confirm: handleClearCart,
+      cancel: closeModal,
+    });
+  }
+
+  function handleClearCart() {
+    clearCart();
+    closeModal();
+  }
+
+  const { clearCart } = useCartAction();
+
+  function closeModal() {
+    setConfirmConfig({ ...initialConfirmConfig, show: false });
+  }
+
   return (
     <>
       <main
@@ -11,9 +56,10 @@ export const Store = () => {
       >
         <ProductList products={products.data} />
       </main>
-      <aside className="fixed top-8 right-0 bg-orange-100 md:block min-h-full">
-        <Cart />
+      <aside>
+        <Cart handleCancel={handleCancel} handlePay={handlePay} />
       </aside>
+      <PopupConfirm {...confirmConfig} />
     </>
   );
 };
