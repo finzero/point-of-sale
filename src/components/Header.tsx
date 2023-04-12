7c0b8b4124ff82027fa65be93d2a88855d963e70
Fazrin <fazrin.mutaqin@wgs-id.com>
2023-04-12 11:45:15 +0700

    header - add padding y

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import orangeImg from '../assets/orange-slice.png';
 
 export const Header = () => {
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 p-2">
       <img src={orangeImg} alt="Logo" className="w-12 h-12" />
       <span className="font-bold text-lg">Orange POS</span>
     </div>
